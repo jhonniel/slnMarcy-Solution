f1972c7290620b9f0beedc9df5c8f850465e41e6
unknown <jhonnielygay99@gmail.com>
2021-09-02 21:14:51 +0800

    marcy

---
+++ slnMarcySolution_Ygay/frmMain.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtItem1 = New System.Windows.Forms.TextBox()
+        Me.txtItem2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(38, 78)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(93, 28)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "Get a discount"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(139, 78)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(60, 28)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(48, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Item 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(152, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Item 2"
+        '
+        'txtItem1
+        '
+        Me.txtItem1.Location = New System.Drawing.Point(38, 41)
+        Me.txtItem1.Name = "txtItem1"
+        Me.txtItem1.Size = New System.Drawing.Size(60, 20)
+        Me.txtItem1.TabIndex = 4
+        '
+        'txtItem2
+        '
+        Me.txtItem2.Location = New System.Drawing.Point(139, 41)
+        Me.txtItem2.Name = "txtItem2"
+        Me.txtItem2.Size = New System.Drawing.Size(60, 20)
+        Me.txtItem2.TabIndex = 5
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(242, 141)
+        Me.Controls.Add(Me.txtItem2)
+        Me.Controls.Add(Me.txtItem1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Name = "frmMain"
+        Me.Text = "Marcy's Departmenstore"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtItem1 As TextBox
+    Friend WithEvents txtItem2 As TextBox
+End Class
---
+++ slnMarcySolution_Ygay/frmMain.vb
@@ -0,0 +1,93 @@
+﻿' Name:         Gross Project
+' Purpose:      Displays gross pay
+' Programmer:   <Jhonniel Ygay> on <Sep 3,2020>
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+
+Public Class frmMain
+    Private Sub txtItem1_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtItem1.Enter
+        txtItem1.SelectAll()
+
+    End Sub
+    Private Sub txtItem2_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles txtItem2.Enter
+        txtItem2.SelectAll()
+
+    End Sub
+    Private Sub txtItem1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtItem1.KeyPress
+
+        If (Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57) Or Asc(e.KeyChar) = 46 Or Asc(e.KeyChar) = 8 Then
+
+        Else
+            e.Handled = True
+        End If
+
+    End Sub
+    Private Sub txtItem2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtItem2.KeyPress
+
+        If (Asc(e.KeyChar) >= 48 And Asc(e.KeyChar) <= 57) Or Asc(e.KeyChar) = 46 Or Asc(e.KeyChar) = 8 Then
+
+        Else
+            e.Handled = True
+        End If
+    End Sub
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtItem1_TextChanged(sender As Object, e As EventArgs) Handles txtItem1.TextChanged
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        System.Threading.Thread.CurrentThread.CurrentCulture = New System.Globalization.CultureInfo("en-US")
+        Dim Item1, Item2, Total, Ftotal As Decimal
+
+        Dim a, b As String
+        Dim dlgok As DialogResult
+
+
+
+        Decimal.TryParse(txtItem1.Text, Item1)
+        Decimal.TryParse(txtItem2.Text, Item2)
+
+        If Item1 > Item2 Then
+            Total = Item2 / 2
+            Ftotal = Total + Item1
+            a = Ftotal.ToString("c2")
+            b = Total.ToString("c2")
+
+            MessageBox.Show("Total: " + a & vbCrLf & "You Save: " + b, "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+        ElseIf Item1 < Item2 Then
+            Total = Item1 / 2
+            Ftotal = Total + Item2
+            a = Ftotal.ToString("c2")
+            b = Total.ToString("c2")
+
+            MessageBox.Show("Total: " + a & vbCrLf & "You Save: " + b, "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Else Item1 = Item2
+            Total = Item2 / 2
+            Ftotal = Total + Item1
+            a = Ftotal.ToString("c2")
+            b = Total.ToString("c2")
+
+            MessageBox.Show("Total: " + a & vbCrLf & "You Save: " + b, "", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        End If
+
+        txtItem1.Focus()
+    End Sub
+
+    Private Sub txtItem2_TextChanged(sender As Object, e As EventArgs) Handles txtItem2.TextChanged
+
+    End Sub
+
+
+
+
+End Class
